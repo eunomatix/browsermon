62e0e1c41e40e781536c143730d120e36c2abdc1
shahzaibjutt <shahzaibrocks9@gmail.com>
2023-07-30 13:23:05 +0500

    writer-interface

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,6 +1,8 @@
+import datetime
 import logging
 import os
 import sys
+from collections import OrderedDict
 
 from src.utils import default_log_loc
 
@@ -52,3 +54,34 @@ def parse_schedule_window(window):
         print(
             "Invalid schedule window format. Please use the valid format (e.g., 1m, 1h, 1d)")  # noqa
         sys.exit(1)
+
+
+def prepare_entry(result, metadata, profile):
+    session_id = result[0]
+    referrer = result[1]
+    url = result[2]
+    title = result[3]
+    visit_time = result[4]
+    visit_count = result[5]
+
+    visit_time_obj = datetime.datetime(1601, 1, 1) + datetime.timedelta(
+        microseconds=visit_time)
+
+    # Create an OrderedDict to maintain the order of keys
+    entry = OrderedDict()
+
+    # Add metadata and profile information at the beginning of the entry
+    entry.update(metadata)
+    entry.update(profile)
+
+    # Add other entry information
+    entry.update({
+        "session_id": session_id,
+        "referrer": referrer,
+        "url": url,
+        "title": title,
+        "visit_time": visit_time_obj.strftime("%Y-%m-%d %H:%M:%S"),
+        "visit_count": visit_count,
+    })
+
+    return entry
