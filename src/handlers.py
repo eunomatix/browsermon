61978b2e5ec72e79a72f424710de6a5faa86ead3
Abdullah Chaudhry <appleconda@gmail.com>
2023-09-30 15:46:24 +0500

    Eunomatix License banner

--- src/handlers.py
+++ src/handlers.py
@@ -1,3 +1,23 @@
+""""/****************************************************************************
+ **
+ ** Copyright (C) 2023 EUNOMATIX
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program. If not, see <https://www.gnu.org/licenses/>.
+ **
+ ** Contact: info@eunomatix.com
+ **
+ **************************************************************************/
+"""
 import os 
 import re
 from apscheduler.triggers.cron import CronTrigger
@@ -113,4 +133,3 @@ class Handler:
         self.logger.info("Cleanup of Handler class")
         self.scheduler.shutdown()
         self.scheduler.remove_all_jobs()
-
